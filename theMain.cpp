e0ee09d19cd7287c1d98502b4d5bdb5d81969917
侑希 今田 <240264@jc-21.jp>
2025-04-25 15:56:34 +0900

    プロジェクト ファイルを追加します。

---
+++ theMain.cpp
@@ -0,0 +1,61 @@
+#include "DxLib.h"
+
+namespace
+{
+	//XGA SIZE
+	const int WIN_WIDTH = 1280;
+	const int WIN_HEIGHT = 720;
+}
+
+
+void DxInit()
+{
+	ChangeWindowMode(true);
+	SetWindowSizeChangeEnableFlag(false, false);
+	SetMainWindowText("TITLE");
+	SetGraphMode(WIN_WIDTH, WIN_HEIGHT, 32);
+	SetWindowSizeExtendRate(1.0);
+	SetBackgroundColor(255, 250, 205);
+
+	// ＤＸライブラリ初期化処理
+	if (DxLib_Init() == -1)
+	{
+		DxLib_End();
+	}
+
+	SetDrawScreen(DX_SCREEN_BACK);
+}
+
+void MyGame()
+{
+
+	DrawFormatString(100, 100, GetColor(0, 0, 0), "ウィンドウのテスト");
+	static int timer = 0;
+	timer++;
+	DrawFormatString(100, 150, GetColor(0, 0, 0), "%010d", timer);
+}
+
+
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
+{
+	DxInit();
+
+	while (true)
+	{
+		ClearDrawScreen();
+
+		//ここにやりたい処理を書く
+
+
+		ScreenFlip();
+		WaitTimer(16);
+		if (ProcessMessage() == -1)
+			break;
+		if (CheckHitKey(KEY_INPUT_ESCAPE) == 1)
+			break;
+	}
+
+	DxLib_End();
+	return 0;
+}
